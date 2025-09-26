5bd322bc4e2c8847b965f7c679cb49d8b6ab755f
Aaron-Moran <aaron.moran@my.jcu.edu.au>
2025-09-26 16:02:43 +1000

    Added Prac 1 files to GitHub repo.

---
+++ prac_02/password_stars.py
@@ -0,0 +1,15 @@
+"""
+CP1404/ Practical 2
+Password stars
+"""
+
+# Minimum length of the password
+MIN_PASSWORD_LENGTH = 8
+
+# Password check loop
+password = input("Password: ")
+while len(password) < MIN_PASSWORD_LENGTH:
+    print("Password is too short!")
+    password = input("Password: ")
+
+print("*" * len(password))
