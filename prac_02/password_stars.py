1f4404b122c0fad6cbbff935d936dcff650e4ebc
Aaron-Moran <aaron.moran@my.jcu.edu.au>
2025-09-26 16:32:42 +1000

    created main function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,14 +2,15 @@
 CP1404/ Practical 2
 Password stars
 """
+def main():
+    # Minimum length of the password
+    MIN_PASSWORD_LENGTH = 8
 
-# Minimum length of the password
-MIN_PASSWORD_LENGTH = 8
-
-# Password check loop
-password = input("Password: ")
-while len(password) < MIN_PASSWORD_LENGTH:
-    print("Password is too short!")
+    # Password check loop
     password = input("Password: ")
+    while len(password) < MIN_PASSWORD_LENGTH:
+        print("Password is too short!")
+        password = input("Password: ")
 
-print("*" * len(password))
+    print("*" * len(password))
+main()
