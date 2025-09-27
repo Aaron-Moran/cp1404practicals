c800ffec111a1938ce9fe66c43ed500eec02762c
Aaron-Moran <aaron.moran@my.jcu.edu.au>
2025-09-27 16:14:47 +1000

    Added a menu loop to choose between manual score evaluation and random score evaluation.

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,6 +2,9 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+MENU = """ E - Evaluate a score
+R - Generate a random score
+Q -Quit """
 
 MIN_SCORE = 0
 MAX_SCORE = 100
@@ -9,8 +12,20 @@ PASSABLE_SCORE = 50
 EXCELLENT_SCORE = 90
 
 def main():
-    score = float(input("Enter score: "))
-    print(get_result(score))
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "E":
+            score = float(input("Enter Score: "))
+            print(get_result(score))
+        elif choice == "R":
+            score = get_random_number
+            print(get_result(score))
+        else:
+            print("invalid value")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Goodbye!")
 
 def get_result(score):
     if score < MIN_SCORE or score > MAX_SCORE:
@@ -21,4 +36,7 @@ def get_result(score):
         return "Passable"
     else:
         return "Bad"
+
+#TODO: Create Random score function
+
 main()
