0f90643274ae9148a14087b225fde60890a1fd6c
Aaron-Moran <aaron.moran@my.jcu.edu.au>
2025-10-05 14:48:38 +1000

    Add numbers.txt and completed part 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,6 +4,7 @@ files.py
 """
 
 NAME_FILENAME = "name.txt"
+NUMBERS_FILENAME = "numbers.txt"
 
 # 1. Ask for a name and write it to name.txt
 name = input("Enter your name: ")
@@ -16,3 +17,9 @@ in_file = open(NAME_FILENAME, "r")
 read_name = in_file.read().strip()
 in_file.close()
 print(f"Hi {read_name}!")
+
+# 3. Read only the first two numbers from numbers.txt, add and print
+with open(NUMBERS_FILENAME, "r") as in_file:
+    first_number = int(in_file.readline())
+    second_number = int(in_file.readline())
+print(first_number + second_number)
