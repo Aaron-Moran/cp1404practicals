1acfcfd74f83b15ff57b5602797713b47c751f7f
Aaron-Moran <aaron.moran@my.jcu.edu.au>
2025-10-09 16:43:13 +1000

    This changes refactors main function, creating the print_report function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -10,15 +10,18 @@ def main():
 
     # get the income for the number of specified months
     for month in range(1, number_of_months + 1):
-        income = float(input(f"Enter income for month {month}")) # Uses f-string instead of string concatenation
+        income = float(input(f"Enter income for month {month} ")) # Uses f-string instead of string concatenation
         incomes.append(income)
 
+    print_report(incomes, number_of_months)
+
+def print_report(incomes: list[any], number_of_months: int):
     # Prints the output from the list
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]     # retrieve income for each month
-        total += income                 # Update the total
+        income = incomes[month - 1]  # retrieve income for each month
+        total += income  # Update the total
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
 main()
