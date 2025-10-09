4d75362aed0d239186ce1e5ee3840af1791de080
Aaron-Moran <aaron.moran@my.jcu.edu.au>
2025-10-09 16:58:12 +1000

    This changes the print_report function to rely on only the incomes list to better match SRP

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -5,23 +5,23 @@ Starter code for cumulative total income program
 
 def main():
     """Display income report for incomes over a given number of months."""
-    incomes = [] # Stores the income for each month
-    number_of_months = int(input("How many months? ")) # number of income inputs
+    incomes = []
+    number_of_months = int(input("How many months? "))
 
-    # get the income for the number of specified months
     for month in range(1, number_of_months + 1):
-        income = float(input(f"Enter income for month {month} ")) # Uses f-string instead of string concatenation
+        income = float(input(f"Enter income for month {month} "))
         incomes.append(income)
 
-    print_report(incomes, number_of_months)
+    print_report(incomes)
 
-def print_report(incomes: list[any], number_of_months: int):
-    # Prints the output from the list
+def print_report(incomes):
+    """Print the income report for the given incomes"""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]  # retrieve income for each month
-        total += income  # Update the total
+    for month in range(1, len(incomes) + 1):
+        income = incomes[month - 1]
+        total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
 
 main()
