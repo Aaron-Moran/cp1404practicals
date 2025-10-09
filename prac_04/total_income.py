b7ae28d2ec3311707840a0fe6646ee2def0cb345
Aaron-Moran <aaron.moran@my.jcu.edu.au>
2025-10-09 15:52:39 +1000

    This changes the variable months to number_of_months

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -6,17 +6,17 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = [] # Stores the income for each month
-    months = int(input("How many months? ")) # number of income inputs
+    number_of_months = int(input("How many months? ")) # number of income inputs
 
     # get the income for the number of specified months
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for month {month}")) # Uses f-string instead of string concatenation
         incomes.append(income)
 
     # Prints the output from the list
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]     # retrieve income for each month
         total += income                 # Update the total
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
