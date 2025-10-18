248ebab34f76a1ed8a2a34f815776484eaab660f
Aaron-Moran <aaron.moran@my.jcu.edu.au>
2025-10-18 20:06:16 +1000

    Complete practical 5 word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,5 +2,26 @@
 CP1404/CP5632 Practical
 Word Occurrences
 Estimate: 30 minutes
-Actual:
+Actual: 26 minutes
 """
+
+def main():
+    text = input("text: ")
+    words = text.lower().split()
+
+    word_to_count = {}
+    for word in words:
+        if word in word_to_count:
+            word_to_count[word] += 1
+        else:
+            word_to_count[word] = 1
+
+    sorted_words = sorted(word_to_count)
+
+    width = max((len(word) for word in sorted_words), default = 0)
+
+    for word in sorted_words:
+        print(f'{word:{width}} : {word_to_count[word]}')
+
+
+main()
