bce870435b4a00fc800cc0a099e55256ea98386d
Aaron-Moran <aaron.moran@my.jcu.edu.au>
2025-11-01 15:31:31 +1000

    This completes the modifications to car.py and used_cars.py

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,13 +4,20 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
+        name: str, the car's name
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name # 6) Now add a name field to the Car class (in car.py)
         self.fuel = fuel
         self._odometer = 0
+
+    # 5) Now add the __str__ method to the Car class in car.py.
+    def __str__(self):
+        """Return string representation of Car."""
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
